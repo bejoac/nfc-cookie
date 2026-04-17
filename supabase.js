582a24365357137d78690d0dfd543b6a35f5af30
Joa <betejoachim@gmail.com>
2026-04-17 16:19:45 +0200

    Fixed script tag and request data from supabase

--- supabase.js
+++ supabase.js
@@ -6,8 +6,10 @@ const url = "https://bovhjfcmyuqdunylkzdq.supabase.co";
 // Create a single supabase client for interacting with your database
 const supabase = createClient(url, apiKey);
 
-if (supabase) {
-  console.log("Success!");
+const { data, error} = await supabase.from("fortune-cookie-sayings").select('*');
+
+if (error) {
+  console.log("Error: ", error);
 } else {
-  console.log("Unsuccess!");
+  console.log("Sucess: ", data);
 };
