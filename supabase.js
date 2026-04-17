92700c23f43934537029c912d0a90670ddd9f916
Joa <betejoachim@gmail.com>
2026-04-17 15:58:10 +0200

    Added simple client implementation for testing purposes

--- supabase.js
+++ supabase.js
@@ -1,1 +1,11 @@
-console.log("Supabase!");
+import { createClient } from '@supabase/supabase-js'
+
+const apiKey = "sb_publishable_bXebsEh1ej_K2vyh73HU1w_SApkXdVi";
+const url = "https://bovhjfcmyuqdunylkzdq.supabase.co";
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient(url, apiKey);
+
+if (supabase) {
+  console.log("Sucess!");
+};
