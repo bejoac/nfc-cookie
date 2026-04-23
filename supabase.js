b26c8496ca9c9328466215c40c1e65079d0ed317
Joa <betejoachim@gmail.com>
2026-04-23 11:08:14 +0200

    Wrapped code in function to stop execution after error occurs

--- supabase.js
+++ supabase.js
@@ -4,16 +4,19 @@ const apiKey = "sb_publishable_bXebsEh1ej_K2vyh73HU1w_SApkXdVi";
 const url = "https://bovhjfcmyuqdunylkzdq.supabase.co";
 const supabase = createClient(url, apiKey);
 
-try {
-  const { data, error } = await supabase.rpc('get_random_item');
+async function loadData() {
+  try {
+    const { data, error } = await supabase.rpc('get_random_item');
 
-  if (error) {
-    console.log("Supabase Error: ", error);
-    return;
+    if (error) {
+      console.log("Supabase Error: ", error);
+      return;
+    };
+
+    document.getElementById("main-container").innerHTML = data.saying;
+  } catch (error) {
+    console.log("Browser API-Call Error: ", error)
   };
-
-  document.getElementById("main-container").innerHTML = data.saying;
-} catch (error) {
-  console.log("Browser API-Call Error: ", error)
 };
 
+loadData();
