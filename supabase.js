7955d5467b3bb2bd84d3f830f9dd8de6c6587168
Joa <betejoachim@gmail.com>
2026-04-24 20:38:52 +0200

    Changed error handling for calling supabase

--- supabase.js
+++ supabase.js
@@ -4,19 +4,17 @@ const apiKey = "sb_publishable_bXebsEh1ej_K2vyh73HU1w_SApkXdVi";
 const url = "https://bovhjfcmyuqdunylkzdq.supabase.co";
 const supabase = createClient(url, apiKey);
 
-async function loadData() {
-  try {
-    const { data, error } = await supabase.rpc('get_random_item');
+try {
+  const { data, error } = await supabase.rpc('get_random_item');
 
-    if (error) {
-      console.log("Supabase Error: ", error);
-      return;
-    };
-
-    document.getElementById("main-container").innerHTML = data.saying;
-  } catch (error) {
-    console.log("Browser API-Call Error: ", error)
+  if (error) {
+    throw new Error("Supabase Error");
   };
+
+  console.log("Test");
+
+  document.getElementById("main-container").innerHTML = data.saying;
+} catch (error) {
+  console.log("Browser API-Call Error: ", error)
 };
 
-setTimeout(loadData, 1000);
